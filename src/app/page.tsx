ec9104b30dc186c5b9fe4b953e3a4fb8161a59e7
Muhammed Salih Gümüş <gumusmuhammedsalih@gmail.com>
2025-01-31 20:50:08 +0300

    disable eslint and during build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function GoogleDriveClone() {
             >
               My Drive
             </Button>
-            {getBreadcrumbs().map((folder, index) => (
+            {getBreadcrumbs().map((folder) => (
               <div key={folder.id} className="flex items-center">
                 <ChevronRight className="mx-2 text-gray-500" size={16} />
                 <Button
@@ -97,7 +97,7 @@ export default function GoogleDriveClone() {
                       </button>
                     ) : (
                       <Link
-                        href={file.url || "#"}
+                        href={file.url ?? "#"}
                         className="flex items-center text-gray-100 hover:text-blue-400"
                       >
                         <FileIcon className="mr-3" size={20} />
